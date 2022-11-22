8ea8927ba078297412dea6f316f995f6ea91baf3
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2022-11-22 10:34:31 +0530

    Create missingNumber.java

---
+++ day-019/missingNumber.java
@@ -0,0 +1,10 @@
+public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int nsum = n*(n+1)/2;
+
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+        }
+        return nsum-sum;
+    }
