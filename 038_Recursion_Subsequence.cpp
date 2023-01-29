d6b4517ca4b46056bf601c93174e361dad99224d
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-01-29 09:30:24 +0530

    Update 038_Recursion_Subsequence.cpp

--- 038_Recursion_Subsequence.cpp
+++ 038_Recursion_Subsequence.cpp
@@ -1,1 +1,14 @@
 
+void printSubsequence(vector<int>&arr , int i , vector<int>s){
+
+  if(i==arr.size()){
+    for(int itm:s)
+    cout<<itm<<" ";
+    cout<<endl;
+    return;
+  }
+    printSubsequence(arr, i+1, s);
+    s.push_back(arr[i]);
+    printSubsequence(arr, i+1, s);
+  
+}
