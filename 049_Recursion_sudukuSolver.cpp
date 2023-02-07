e748dc0b124b8ab5d3759aebc07a96efac5da200
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-02-07 16:16:50 +0530

    Create 049_Recursion_sudukuSolver.cpp

---
+++ 049_Recursion_sudukuSolver.cpp
@@ -0,0 +1,32 @@
+    bool isValid(vector<vector<char>>&board , int row,int col,char ch){
+        for(int i=0; i<=8; i++){
+            if(board[row][i]==ch)return false;
+            if(board[i][col]==ch)return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch)return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>&board){
+        for(int row=0; row<board.size(); row++){
+            for(int col=0; col<board[0].size(); col++){
+                if(board[row][col]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isValid(board,row,col,ch)){
+                            board[row][col]=ch;
+                            
+                            bool check=solve(board);
+                            if(check==true)
+                               return true;
+                            else
+                               board[row][col]='.';
+                        }
+                    }
+                    return false;
+                }  
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
