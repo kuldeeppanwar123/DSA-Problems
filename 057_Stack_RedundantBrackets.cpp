fa3aec8eed01a361b422a7d3be99e82691a4ee70
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-02-17 16:40:51 +0530

    Create 057_Stack_RedundantBrackets.cpp

---
+++ 057_Stack_RedundantBrackets.cpp
@@ -0,0 +1,20 @@
+int checkRedundancy(string s) {
+        // code here
+        stack<char>st;
+   for(int i=0; i<s.length(); i++){
+    if(s[i]=='(' || s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/'){
+        st.push(s[i]);
+    }
+    else if(s[i]==')'){
+        if(st.top()=='(')
+          return 1;
+        else{
+            while (st.top()!='(')
+              st.pop();
+            
+            st.pop();           
+        }
+    }
+   }
+   return 0;
+    }
