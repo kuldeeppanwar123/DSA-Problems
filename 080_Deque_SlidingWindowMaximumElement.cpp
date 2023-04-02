4695659383d2e6746ce7b1d6a42dcb102d88ae3d
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-04-02 22:37:22 +0530

    Create 080_Deque_SlidingWindowMaximumElement.cpp

---
+++ 080_Deque_SlidingWindowMaximumElement.cpp
@@ -0,0 +1,26 @@
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>ans;
+        deque<int>dq;
+        
+        for(int i=0; i<k; i++){
+            while(!dq.empty() && nums[i]>=nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+
+        ans.push_back(nums[dq.front()]);
+
+        for(int i=k; i<nums.size(); i++){
+            while(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[i]>=nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+
+            ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
