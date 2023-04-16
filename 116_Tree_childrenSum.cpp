ba8d672c4040eff0b8fd6a0ec30f270ec4587050
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-04-16 08:37:52 +0530

    Update 116_Tree_childrenSum.cpp

--- 116_Tree_childrenSum.cpp
+++ 116_Tree_childrenSum.cpp
@@ -1,28 +1,37 @@
-//remaining/panding question
-
-int isSum(TreeNode*node){
-    if(node==NULL) return;
-    int sum=0;
-  
-  if(node->left!=NULL)sum+=node->left->data;
-  if(node->right!=NULL) sum+=node->right->data;
-  
-  if(sum>=node->data)
-    node->val == sum;
-  else{
-    if(node->left!=NULL) node->left->data = node->data;
-    if(node->right!=NULL) node->right->node->right->data = sum;
-  }
-  
-  isSum(node->left);
-  isSum(node->right);
-  
-  int tota = 0;
-  if(node->left!=NULL) 
+void isSumProperty(TreeNode*node){
+    if(node==NULL)
+       return;
+    
+    int sum = 0;
+    if(node->left!=NULL)
+      sum+=node->left->data;
+    
+    if(node->right!=NULL)
+      sum+=node->right->data;
+      
+    if(sum>=node->data)
+      node->data = sum;
+      
+    else if(sum<node->data)
+    {
+        if(node->left!=NULL)
+          node->left->data = sum;
+          
+        if(node->right!=NULL)
+          node->right->data = sum;
+    }
+    
+    isSumProperty(node->left);
+    isSumProperty(node->right);
+    
+    int total = 0;
+    if(node->left!=NULL)
     total+=node->left->data;
-        
-  if(node->right!=NULL)
-  {
-      total+=node->right->data);
-  
-  }
+    
+    if(node->right!=NULL)
+    total+=node->right->data;
+    
+    //check if node is leaf
+    if(node->left!=NULL && node->right!=NULL) 
+    node->data = total;
+}
