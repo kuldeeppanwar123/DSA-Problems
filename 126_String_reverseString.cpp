ae6cd29d512165248be3d4a66fa58ca200660417
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-04-27 09:51:12 +0530

    Create 126_String_reverseString.cpp

---
+++ 126_String_reverseString.cpp
@@ -0,0 +1,9 @@
+void reverseString(vector<char>& str) {
+        int s =0;
+        int e = str.size()-1;
+        while(s<=e){
+            swap(str[s],str[e]);
+            s++;
+            e--;
+        }
+    }
