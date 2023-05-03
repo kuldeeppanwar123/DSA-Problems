c19d74969fb65f3063a6d6fc57b864af22b36a6c
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-05-03 16:01:32 +0530

    Update 131_String_patternMatching.cpp

--- 131_String_patternMatching.cpp
+++ 131_String_patternMatching.cpp
@@ -3,7 +3,7 @@ bool searchPattern(string str, string pat)
     // your code here
    int isfound =  str.find(pat);
    if(isfound==string::npos)
-   return false;
-   
+       return false;
+    
    return true;
 }
