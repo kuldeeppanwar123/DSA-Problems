fcd987a669fedf7a89111c133ce1557eb08b2700
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-05-15 18:57:36 +0530

    Create 142_String_uniquePermutationOfString.cpp

---
+++ 142_String_uniquePermutationOfString.cpp
@@ -0,0 +1,24 @@
+class Solution
+{ set<string>Set;
+	public:
+	    void permutation(string s , int idx){
+	        if(idx==s.length()){
+	            Set.insert(s);
+	            return;
+	        }
+	        for(int i=idx; i<s.length(); i++){
+	            swap(s[i] , s[idx]);
+	            permutation(s , idx+1);
+	            swap(s[i] , s[idx]);
+	        }
+	    }
+		vector<string>find_permutation(string S)
+		{
+		    permutation( S ,0);
+		    vector<string>ans;
+		    for(string s:Set){
+		        ans.push_back(s);
+		    }
+		    return ans;
+		}
+};
