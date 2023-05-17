09be5fdafce18382d923c77d7b23910281c53598
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-05-17 21:42:38 +0530

    Update 143_String_anagramSearch.cpp

--- 143_String_anagramSearch.cpp
+++ 143_String_anagramSearch.cpp
@@ -19,3 +19,40 @@ int search(string pat, string txt) {
         }
         return true;
     }
+
+
+// intuition -2 (Optimal solution)
+bool isMatch(int patcount[] , int txtcount[]){
+        for(int i=0; i<26; i++){
+            if(patcount[i]!=txtcount[i])
+             return false;
+        }
+        return true;
+    }
+    
+	int search(string pat, string txt) {
+	    int count =0;
+	    
+	    int m = txt.size();
+	    int n = pat.size();
+	    
+	    int patcount[26] = {0};
+	    int txtcount[26] = {0};
+	    
+	    for(int i=0; i<n; i++){
+	        patcount[pat[i]-'a']++;
+	        txtcount[txt[i]-'a']++;
+	    }
+	    
+	    for(int i=n; i<m; i++){
+	        if(isMatch(patcount , txtcount)){
+	            count++;
+	        }
+	        txtcount[txt[i]-'a']++;
+	        txtcount[txt[i-n]-'a']--;
+	    }
+	    if(isMatch(patcount , txtcount)){
+	            count++;
+	        }
+	   return count; 
+	}
