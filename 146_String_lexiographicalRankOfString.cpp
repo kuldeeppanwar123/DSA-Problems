37a2699cfe721b03e6c67325c6b8536802e72c6e
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-05-20 13:07:06 +0530

    Update 146_String_lexiographicalRankOfString.cpp

--- 146_String_lexiographicalRankOfString.cpp
+++ 146_String_lexiographicalRankOfString.cpp
@@ -1,11 +1,35 @@
-long long factorial(long long n){
-        long long ans = 1;
-        for(long long i=1; i<=n; i++){
-            ans*=i;
-        }
-        return ans;
-    }
-    
+// intuition-1 (Naive)
+
+int findSmallerInRight(string str, int low, int high)
+{
+	int countRight = 0, i;
+	for (i = low + 1; i <= high; ++i)
+		if (str[i] < str[low])
+			++countRight;
+	return countRight;
+}
+
+int findRank(string str)
+{
+	int len = str.size();
+	int mul = fact(len);
+	int rank = 1;
+	int countRight;
+	int i;
+	for (i = 0; i < len; ++i) {
+		mul /= len - i;
+		// Count number of chars smaller than str[i]
+		// from str[i+1] to str[len-1]
+		countRight = findSmallerInRight(str, i, len - 1);
+                rank += countRight * mul;
+	}
+	return rank;
+}
+
+
+
+
+// intuition-2 (Optimal)    
     long long findRank(string str) {
         int n= str.size();
         int count[256] = {0};
