30b037bca896fa3d98cea19b40754470aa6f0dca
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-05-26 11:49:52 +0530

    Create 152_BST_searchingInBST.cpp

---
+++ 152_BST_searchingInBST.cpp
@@ -0,0 +1,12 @@
+TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL)
+        return NULL;
+
+        if(root->val==val)
+        return root;
+
+        else if(root->val < val)
+        return searchBST(root->right , val);
+        else
+        return searchBST(root->left , val);
+    }
