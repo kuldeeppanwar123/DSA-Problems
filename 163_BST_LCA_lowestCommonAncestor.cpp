2353b3d4138c3f677471d1aef3426f4663a3ce16
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-05-31 22:44:33 +0530

    Create 163_BST_LCA_lowestCommonAncestor.cpp

---
+++ 163_BST_LCA_lowestCommonAncestor.cpp
@@ -0,0 +1,12 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)return NULL;
+        int curr = root->val;
+
+        if(curr>p->val && curr >q->val)
+        return lowestCommonAncestor(root->left , p,q);
+
+        if(curr<p->val && curr<q->val)
+        return lowestCommonAncestor(root->right , p,q);
+
+        return root;
+    }
