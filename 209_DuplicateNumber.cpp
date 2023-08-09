390fefcbe1e30bc288514094b8b99f155f337fb9
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-08-09 11:17:44 +0530

    Create 209_DuplicateNumber.cpp

---
+++ 209_DuplicateNumber.cpp
@@ -0,0 +1,51 @@
+// approach 0
+using (N^2)
+
+// approach 1
+int findDuplicate(vector<int>& nums) {
+        vector<int>map(nums.size()+1,0);
+        int ans;
+        for(int i:nums){
+            if(map[i]==1){
+               ans = i;
+               break;
+            }
+            map[i]++;
+        }
+        return ans;
+        
+    }
+
+
+
+// approach 2
+int findDuplicate(vector<int>& nums) {
+        unordered_set<int>set;
+        int ans;
+        for(int item:nums){
+            if(set.find(item)!=set.end()){
+                ans =  *set.find(item);
+                break;
+            }
+            set.insert(item);
+        }
+        return ans;
+    }
+
+// approach 3
+int findDuplicate(vector<int>& nums) {
+        int slow = 0; 
+        int fast = 0;
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+
+        slow=0;
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
