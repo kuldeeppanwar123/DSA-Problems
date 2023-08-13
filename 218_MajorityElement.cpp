69f999802784ca22e4059939232efa43b3b189df
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-08-13 19:01:43 +0530

    Create 218_MajorityElement.cpp

---
+++ 218_MajorityElement.cpp
@@ -0,0 +1,20 @@
+int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int ele = nums[0];
+        int count = 1;
+
+        for(int i=1; i<n; i++){
+            if(nums[i]==ele)
+            count++;
+
+            else{
+                if(count==0){
+                    count = 1;
+                    ele = nums[i];
+                }
+                else
+                    count--;
+            }
+        }
+        return ele;
+    }
